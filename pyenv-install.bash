4d578881e11885413e70a3c41a997dbc04032a5b
Sapjarern <Sapjarern081@gmail.com>
2024-03-13 11:31:04 +0700

    remove secert and update script

--- pyenv-install.bash
+++ pyenv-install.bash
@@ -1,7 +1,11 @@
-sudo apt-get update; sudo apt-get install make build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
-rm -rf ~/.pyenv
+sudo apt-get update && sudo apt-get install make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev \
+libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
+
+if [ -d '~/.pyenv' ]; then
+    rm -rf ~/.pyenv
+fi
+
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 cd ~/.pyenv && src/configure && make -C src
 
